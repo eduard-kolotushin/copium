3b9d2de5b2e346dc92763ed40854886accf7d9f1
Eduard Kolotushin <kolotushin.ev@phystech.edu>
2023-08-01 23:17:26 +0300

    Start progress

--- app/model.py
+++ app/model.py
@@ -3,16 +3,21 @@ import sys
 from flask_security import RoleMixin, UserMixin, AsaList
 from sqlalchemy.ext.mutable import MutableList
 import config
-from sqlalchemy import create_engine, ForeignKey, String, Column, Integer
+from sqlalchemy import create_engine, ForeignKey, String, Column, Integer, Table
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.declarative import DeferredReflection
 from sqlalchemy.orm import DeclarativeBase, scoped_session, sessionmaker, relationship, backref
 from utils import create_logger
 
 logger = create_logger(__name__)
 
 
-class Based(DeclarativeBase):
+class Base(DeclarativeBase):
     pass
+
+
+class Reflected(DeferredReflection):
+    __abstract__ = True
 
 
 try:
@@ -25,24 +30,27 @@ except Exception as e:
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-Based.query = db_session.query_property()
+Base.query = db_session.query_property()
 
 
-class UserModel(Based, UserMixin):
+class UserModel(Reflected, Base, UserMixin):
     __tablename__ = 'users'
-    roles = relationship('Role', secondary='roles_users',
-                         backref=backref('users', lazy='dynamic'))
+    id = Column(Integer(), primary_key=True)
+    roles = relationship('Role', secondary="roles_users",
+                         back_populates="users")
 
 
-class Role(Based, RoleMixin):
+class Role(Base, RoleMixin):
     __tablename__ = 'role'
     id = Column(Integer(), primary_key=True)
     name = Column(String(80), unique=True)
     description = Column(String(255))
     permissions = Column(MutableList.as_mutable(AsaList()), nullable=True)
+    users = relationship('UserModel', secondary="roles_users",
+                         back_populates="roles")
 
 
-class RolesUsers(Based):
+class RolesUsers(Reflected, Base):
     __tablename__ = 'roles_users'
     id = Column(Integer(), primary_key=True)
     user_id = Column('user_id', Integer(), ForeignKey('users.id'))
@@ -50,10 +58,17 @@ class RolesUsers(Based):
 
 
 # Reflect tables from database, exit on fail
-Base = automap_base(declarative_base=Based)
 try:
-    Base.prepare(autoload_with=engine)
-    PublicationModel = Base.classes.publications
+    Reflected.prepare(engine)
+    Base.metadata.reflect(engine, only=["publications"])
 except Exception as e:
     logger.error("Couldn't reflect tables from database", exc_info=e, stack_info=True)
     sys.exit(1)
+
+
+class PublicationModel(Base):
+    __table__ = Base.metadata.tables["publications"]
+
+
+user = UserModel()
+print(user.roles)
